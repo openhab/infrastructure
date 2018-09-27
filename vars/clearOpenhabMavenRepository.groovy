9c734f667167897acd9587afac76b6f7740510f4
Patrick Fink <pfink@users.noreply.github.com>
2018-09-27 18:23:19 +0200

    Workaround: Clear repo times because sometimes first delete is not correctly applied
    Signed-off-by: Patrick Fink <mail@pfink.de>

--- vars/clearOpenhabMavenRepository.groovy
+++ vars/clearOpenhabMavenRepository.groovy
@@ -1,5 +1,9 @@
 def call(String repositoryId) {
-    withCredentials([usernamePassword(credentialsId: "openhab-sandbox-deployer", passwordVariable: 'jfrogPassword', usernameVariable: 'jfrogUser')]) {
-        sh 'curl -u '+ jfrogUser + ':' + jfrogPassword +' -X "DELETE" ' + "https://openhab.jfrog.io/openhab/" + repositoryId + "/"
+    withCredentials([usernamePassword(credentialsId: "openhab-sandbox-deployer", passwordVariable: 'jfrogPassword', usernameVariable: 'jfrogUser')]) {        
+        // Workaround: Delete two times because sometimes first delete is not correctly applied
+        for (i = 0; i < 2; i++) {
+           sh 'curl -u '+ jfrogUser + ':' + jfrogPassword +' -X "DELETE" ' + "https://openhab.jfrog.io/openhab/" + repositoryId + "/"
+           sh "sleep 300"
+        }
     }
 }
