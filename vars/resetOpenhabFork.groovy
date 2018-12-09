8d173521b48f14dc6fd4f7e834c206d34e2d04fd
Patrick Fink <mail@pfink.de>
2018-12-09 20:47:36 +0100

    Use openHAB Repo as a base for ESH as well
    Signed-off-by: Patrick Fink <mail@pfink.de>

--- vars/resetOpenhabFork.groovy
+++ vars/resetOpenhabFork.groovy
@@ -1,7 +1,8 @@
 def call(String gitForkBaseUrl, String componentName, String removeTagIfExists = "") {
 	withCredentials([usernamePassword(credentialsId: env.GIT_CREDENTIALS_ID, passwordVariable: 'gitPassword', usernameVariable: 'gitUser')]) {
 		def gitForkUrl = gitForkBaseUrl + "/" + componentName + ".git"
-		def gitBaseUrl = (componentName == "smarthome" ? "https://github.com/eclipse" : "https://github.com/openhab") + '/' + componentName
+		def gitBaseUrl = "https://github.com/openhab/" + componentName
+		
 		if(!env.SKIP_SANDBOX_RESET?.toBoolean()) {			
 			resetGitFork(gitForkUrl, gitBaseUrl, gitUser, gitPassword)			
 		}
