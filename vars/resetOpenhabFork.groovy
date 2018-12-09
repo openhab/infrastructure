2d5ee3c05333aa2030f2afb8d1c4295c74a443f2
Patrick Fink <mail@pfink.de>
2018-12-09 17:52:58 +0100

    Add remove tag option to openHAB fork reset
    Signed-off-by: Patrick Fink <mail@pfink.de>

--- vars/resetOpenhabFork.groovy
+++ vars/resetOpenhabFork.groovy
@@ -1,11 +1,12 @@
-def call(String gitForkBaseUrl, String componentName) {
-	if(!env.SKIP_SANDBOX_RESET?.toBoolean()) {
+def call(String gitForkBaseUrl, String componentName, String removeTagIfExists = "") {
+	withCredentials([usernamePassword(credentialsId: env.GIT_CREDENTIALS_ID, passwordVariable: 'gitPassword', usernameVariable: 'gitUser')]) {
 		def gitForkUrl = gitForkBaseUrl + "/" + componentName + ".git"
-		def gitBaseUrl = componentName == "smarthome" ? "https://github.com/eclipse" : "https://github.com/openhab"
-		gitBaseUrl = gitBaseUrl + '/' + componentName
-
-		withCredentials([usernamePassword(credentialsId: env.GIT_CREDENTIALS_ID, passwordVariable: 'gitPassword', usernameVariable: 'gitUser')]) {
-			resetGitFork(gitForkUrl, gitBaseUrl, gitUser, gitPassword)
+		def gitBaseUrl = (componentName == "smarthome" ? "https://github.com/eclipse" : "https://github.com/openhab") + '/' + componentName
+		if(!env.SKIP_SANDBOX_RESET?.toBoolean()) {			
+			resetGitFork(gitForkUrl, gitBaseUrl, gitUser, gitPassword)			
+		}
+		if(removeTagIfExists != "") {
+			deleteGitTag(gitForkUrl, removeTagIfExists, gitUser, gitPassword)
 		}
 	}
 }
